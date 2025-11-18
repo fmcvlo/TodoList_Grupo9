ce365c30a93988c36937af16e31587e1cdadd198
Julian Gonzalez <julian.gchiquirrin@gmail.com>
2025-11-18 01:27:36 -0300

    fix: desactiva el modo oscuro por default

--- app/src/main/java/ro/alexmamo/roomjetpackcompose/ui/theme/Theme.kt
+++ app/src/main/java/ro/alexmamo/roomjetpackcompose/ui/theme/Theme.kt
@@ -29,7 +29,7 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun RoomJetpackComposeTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
