6dcda626c9b502cc05e7219035ef7a9197c19f72
amendo <amendo1964@gmail.com>
2025-11-18 22:45:38 -0300

    calender transactions ok

--- app/src/main/java/ro/alexmamo/roomjetpackcompose/presentation/swap/TransactionsScreen.kt
+++ app/src/main/java/ro/alexmamo/roomjetpackcompose/presentation/swap/TransactionsScreen.kt
@@ -1,17 +1,21 @@
 package ro.alexmamo.roomjetpackcompose.presentation.swap
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -36,9 +40,12 @@ import ro.alexmamo.roomjetpackcompose.presentation.home.mapper.TransactionUiMapp
 import ro.alexmamo.roomjetpackcompose.ui.theme.MainGreen
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.sp
+import ro.alexmamo.roomjetpackcompose.R
 import ro.alexmamo.roomjetpackcompose.components.ScreenHeader
 import ro.alexmamo.roomjetpackcompose.presentation.swap.components.TransactionsBalance
+import ro.alexmamo.roomjetpackcompose.ui.theme.DarkText
 
 @Composable
 fun TransactionsScreen(
@@ -110,14 +117,24 @@ fun TransactionsScreenContent(
                         contentPadding = PaddingValues(bottom = 100.dp)
                     ) {
                         item {
+                        Row(horizontalArrangement = Arrangement.SpaceBetween,
+                            modifier = Modifier.fillMaxWidth())
+                        {
                             Text(
                                 text = "April",
                                 fontSize = 16.sp,
                                 color = Color.Gray,
                                 modifier = Modifier
                                     .padding(bottom = 8.dp)
-                                    .align(Alignment.Start)
                             )
+                            Icon(
+                                painter = painterResource(id = R.drawable.calender),
+                                contentDescription = "Calendar",
+                                modifier = Modifier.size(30.dp),
+                                tint= Color.Unspecified
+                            )
+                        }
+
                         }
                         items(transactions) { transaction ->
                             TransactionItem(transaction = transaction)
